fe97fb68c6bddfdb2232a7b84a9f93d88df8488e
ClemRea <clementrea7@gmail.com>
2023-10-17 11:57:53 +0200

    Updates

---
+++ precache-manifest.14c25a3c68185b64c67bc1c5858e503a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5656de9088caba1b2bbe1f82108515b6",
+    "url": "/To_Do_List/index.html"
+  },
+  {
+    "revision": "1ee7a117b5f1115a1759",
+    "url": "/To_Do_List/static/css/main.758954ac.chunk.css"
+  },
+  {
+    "revision": "0280e64d4fdd84034fdf",
+    "url": "/To_Do_List/static/js/2.26d2803c.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/To_Do_List/static/js/2.26d2803c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1cb3552869f0402de0c8",
+    "url": "/To_Do_List/static/js/3.4cc16029.chunk.js"
+  },
+  {
+    "revision": "1ee7a117b5f1115a1759",
+    "url": "/To_Do_List/static/js/main.7548835c.chunk.js"
+  },
+  {
+    "revision": "f2d26ba8e2eedc54165f",
+    "url": "/To_Do_List/static/js/runtime-main.521eef4c.js"
+  }
+]);
